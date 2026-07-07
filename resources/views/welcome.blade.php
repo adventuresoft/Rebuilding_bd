ce1abe6b1ebeb6336f5be129b472c48df295135e
TareqJamilSarkar <tareqjamilsarkar@gmail.com>
2026-07-07 16:10:14 +0600

    chore: update website title to Rebuilding Bangladesh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>July 24 Fighter & Citizen Management Portal</title>
+    <title>Rebuilding Bangladesh</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
